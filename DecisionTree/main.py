931e2715a2089d4e69fa9471852d0371ce831dd8
Albertini96 <guilhermeoliveira@arttu.co>
2020-04-21 19:22:42 -0300

    tree

--- DecisionTree/main.py
+++ DecisionTree/main.py
@@ -1,24 +1,35 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
-from math import log2
+from numpy import log2
 import operator
 
 
 #Calculates simple entropy
 #0 if all samples has same y
-#0 if all samples has mixed y
-def calc_entropy(p, n):
-#    return (-p / (p+n)) * (log2(p / (p+n))) - (n/(p+n)) * (log2(n / (p+n)))
-    return (-p / (p+n)) * (log2( (p / (p+n)) +1)) - (n/(p+n)) * (log2( n / (p+n) +1))
+#1 if all samples has mixed y
+def calc_entropy(p_dict):
+    ret = 0
+    s = 0
+    for i in p_dict:
+        s = s + p_dict[i]
+    
+    for i in p_dict:
+        ret = ret + ((-p_dict[i]/s) * log2((p_dict[i]/s)))
+    
+    return ret
+
 
 #Calculte entropy based on last column of dataset
 def get_entropy(ds):
+    lst = ds.iloc[:, - 1].unique()
+    dict_p = dict()
     
-    p = ds[ds.iloc[:, - 1] == 1].count()[0]
-    n = ds[ds.iloc[:, - 1] == 0].count()[0]
+    for i in lst:
+        dict_p[i] = ds[ds.iloc[:, - 1] == i].count()[0]
     
-    return calc_entropy(p, n), [p, n]
+    return calc_entropy(dict_p), dict_p
+
 
 #Calculate entropy foreach value of each column
 def get_all_entropy(ds):
@@ -48,13 +59,23 @@ def get_info(ds):
     ds_entropy, ds_pn = get_entropy(ds)
     ds_details = get_all_entropy(ds)
     
+    den = 0
+    for i in ds_pn:
+        den = den + ds_pn[i]
+    
     for col in ds_details:
         vals = ds_details.get(col)
 
         temp_list = list()
         for i in vals:    
             it = vals.get(i)
-            temp_list.append( ((it['pn'][0] + it['pn'][1]) / (ds_pn[0] + ds_pn[1])) * it['entropy'] )
+            
+            nom = 0
+            
+            for c in it['pn']:
+                nom = nom + it['pn'][c] 
+                
+            temp_list.append(( abs(nom) / abs(den) )* it['entropy'] )
             info_dict[col] = sum(temp_list)
             
 
@@ -75,11 +96,32 @@ def calculate_gain(ds):
 
     return sorted_d
 
+
+def create_decision_tree(ds):
+    
+    ds_gain = calculate_gain(ds)
+
+    tree_dict = dict()
+    
+    curr_col = list(ds_gain.keys())[0]    
+    unique_vals = ds[curr_col].unique().tolist()
+    
+    print(unique_vals)
+    for i in ds_gain:
+        print(i)
+
+
 ds = pd.read_excel('Base.xlsx')
 
-print(get_all_entropy(ds))
-print(get_info(ds))
-print(calculate_gain(ds))
+create_decision_tree(ds)
+
+#print(ds)
+#print()
+#print(get_all_entropy(ds))
+#print()
+#print(get_info(ds))
+#print()
+#print(calculate_gain(ds))
 
     
     
